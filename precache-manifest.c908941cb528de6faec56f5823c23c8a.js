b22de018c1ea5700a2c7fcf5b1e5d0f5b179c257
Olzhas Kuanov <olzhas.kuanov@gmail.com>
2020-08-02 20:30:28 +0600

    Updates

---
+++ precache-manifest.c908941cb528de6faec56f5823c23c8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4051d9ee7e169d707244fe610e5ec1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a36439ce629521555849",
+    "url": "/static/js/2.e8325983.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.e8325983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b979bea1296f8324ddd2",
+    "url": "/static/js/main.223eb6e0.chunk.js"
+  },
+  {
+    "revision": "28ff25d385eae9fdf318",
+    "url": "/static/js/runtime-main.ad180c90.js"
+  }
+]);
