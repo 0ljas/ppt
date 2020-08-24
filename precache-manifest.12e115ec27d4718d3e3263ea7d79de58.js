9db19a279ab02ebe7026bb8c1b846d0e8129d302
Olzhas Kuanov <olzhas.kuanov@gmail.com>
2020-08-24 16:00:22 +0600

    Updates

---
+++ precache-manifest.12e115ec27d4718d3e3263ea7d79de58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec21b481552e3655331674493dbd17a4",
+    "url": "/ppt/index.html"
+  },
+  {
+    "revision": "9180ccd9aa0ea15641fc",
+    "url": "/ppt/static/js/2.5d91c27b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/ppt/static/js/2.5d91c27b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f4ba466fd2e521ec3f",
+    "url": "/ppt/static/js/main.a1344c70.chunk.js"
+  },
+  {
+    "revision": "b8f886f5a3897a0be51d",
+    "url": "/ppt/static/js/runtime-main.55cd4bf9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ppt/precache-manifest.8b06a49f1cf107128dc2d19c9a5f80c0.js"
+  "/ppt/precache-manifest.12e115ec27d4718d3e3263ea7d79de58.js"
 );
 
 self.addEventListener('message', (event) => {
